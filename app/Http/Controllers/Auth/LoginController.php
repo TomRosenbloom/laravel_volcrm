6a2be4a70ba2f1978d41074c7e3084f6b48412b9
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-03-23 13:11:09 +0000

    added flash messages for login/logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+use Session;
+
 class LoginController extends Controller
 {
     /*
@@ -25,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'dashboard';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +38,31 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * overide authenticated method to insert flash message
+     *
+     * @param  Request $request [description]
+     * @param  [type]  $user    [description]
+     * @return [type]           [description]
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        $request->session()->flash('success', $user->name . ' logged in');
+    }
+
+    /**
+     * overide logout method to insert flash message
+     *
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+        $request->session()->flash('success', 'You have logged out');
+        return redirect($this->redirectTo);
     }
 }
