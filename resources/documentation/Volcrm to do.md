d046e5a669ba3021fdcb1008e9db1e9be8bc0a24
TomRosenbloom <TomRosenbloom@users.noreply.github.com>
2018-06-15 17:55:26 +0100

    db config for heroku cleardb

--- resources/documentation/Volcrm to do.md
+++ resources/documentation/Volcrm to do.md
@@ -2,5 +2,8 @@
 
 Create email alerts to self to log activity i.e. any admin registrations or logins and data changes so I can keep on top of the state of the data.
 
+Dismiss confirmation messages
 
+Table of cities, drop down in address form (lift from laravel contacts)
 
+Correct way to add js - compiling to app.js (as per css)
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,9 @@
 
 @section('content')
 
+<div class="w-75">
     <h1>About this demo app</h1>
+    <h2>Intro</h2>
     <p>
         This is a demo application that I'm making to develop and showcase my knowledge of Laravel and related technologies
     </p>
@@ -17,6 +19,22 @@
     <p>
         Any registrations, logins and data changes are logged and emailed to me in case anyone decides to trash the place.
     </p>
-
+    <p>
+        The use case is a CRM and volunteer matching system for the voluntary sector. The core is a database of organisations
+        offering voluntary roles (or doing other work in the sector). Public users can browse the database,
+        admin users can add, edit and delete organisations.
+    </p>
+    <p>
+        Ultimately, the plan is that people will be able to
+        <ul>
+            <li>Register as a volunteer, create a profile and then search
+            for roles and receive automated alerts</li>
+            <li>Register as an organisational user, and then create and manage an organisation/its voluntary roles</li>
+        </ul>
+    </p>
+    <h2>Features</h2>
+    <h3>CRUD for organisations</h3>
+    <p></p>    
+</div>
 
 @stop
