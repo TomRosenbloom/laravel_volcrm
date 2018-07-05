8c62af11523c9c3e83b0c8303ab9d4b247f6c51c
TomRosenbloom <TomRosenbloom@users.noreply.github.com>
2018-07-05 14:27:22 +0100

    logging config for heroku

--- config/logging.php
+++ config/logging.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'heroku'),
+    'default' => env('LOG_CHANNEL', 'errorlog'),
 
     /*
     |--------------------------------------------------------------------------
